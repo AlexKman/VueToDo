978f15ac6614f319606809f7b78fd7ce38f5dbea
Alex <keeley.lp@gmail.com>
2022-06-23 21:03:58 +0100

    Updated vue to do component with flex div with text input and submit button

--- src/components/VueToDo.vue
+++ src/components/VueToDo.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="container">
     <h1 class="text-center mt-8">My app</h1>
+    <div class="d-flex">
+      <input type="text" placeholder="Enter Task" class="form-control" />
+      <button class="btn btn-warning rounded-0">Submit</button>
+    </div>
   </div>
 </template>
 
